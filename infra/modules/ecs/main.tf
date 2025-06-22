8b2fb28bd67bbef1b2ef33736e9a69d2be7e7051
Viraj Dalave <virajdvd7@gmail.com>
2025-06-22 23:43:43 +0530

    Update main.tf

--- infra/modules/ecs/main.tf
+++ infra/modules/ecs/main.tf
@@ -100,7 +100,7 @@ resource "aws_autoscaling_group" "ecs_instances" {
   name = "ecs-asg"
   min_size = 2
   max_size = 4
-  desired_capacity = 2
+  desired_capacity = 4
   vpc_zone_identifier = var.private_subnets
   health_check_type         = "EC2"
   health_check_grace_period = 300
